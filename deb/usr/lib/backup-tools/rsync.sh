5792df452b2cac29da224c834563a7a17a781edc
Ildar Bikmamatov <vistoyn@gmail.com>
2016-09-12 15:12:28 +0600

    change url

--- deb/usr/lib/backup-tools/rsync.sh
+++ deb/usr/lib/backup-tools/rsync.sh
@@ -1,10 +1,10 @@
 ###############################################################################
 #                               Backup Tools                                  #
 #                                                                             #
-# https://github.com/vistoyn/backup_tools                                     #
+# https://github.com/vistoyn/backup-tools                                     #
 # Copyright (c) 2016 Ildar Bikmamatov <vistoyn@gmail.com>                     #
 # Licensed under the MIT License                                              #
-# (https://github.com/vistoyn/backup_tools/blob/master/LICENSE)               #
+# (https://github.com/vistoyn/backup-tools/blob/master/LICENSE)               #
 #                                                                             #
 ###############################################################################
 
